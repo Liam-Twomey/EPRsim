0685fe3491c0e5092ae90df57c58a0111c09a8b2
Liam Twomey <lptwomey@ucdavis.edu>
2024-11-25 19:29:28 -0800

    initiate testing framework

---
+++ tests/eprload_test.py
@@ -0,0 +1,7 @@
+import pytest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir)) # allows importing files from parent directory
+
+from EPRsim_src import eprload
+
