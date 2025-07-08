96bfec594170ae00c84011774e0791d4cc495aee
Liam Twomey <lptwomey@ucdavis.edu>
2025-07-08 14:13:21 -0700

    Add EPRplot for custom matplotlib settings

---
+++ src/EPRplot.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import matplotlib_inline
+from matplotlib.pyplot import rcParams
+matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
+rcParams["font.family"] = "Arial"
+rcParams['font.size'] = 12
+rcParams["savefig.bbox"] = 'tight'
+rcParams["figure.autolayout"]=True
+
+pal2 = sns.color_palette(['#000000','#023eff','#ff7c00','#1ac938','#e8000b','#8b2be2','#9f4800', '#f14cc1','#ffc400','#00d7ff'])
+sns.set_palette(pal2)
